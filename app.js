c50b7867ff68c7cbb19328012b1cbe67531482e0
Juan Coronado <72060210+JuanJCoronado@users.noreply.github.com>
2023-07-09 19:25:16 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,115 +1,97 @@
 console.log("Confirm that this is working")
 
-let url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
+const URL = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-// -----------------Bar Chart--------------------
+function optionChanged(SubjectID) {
+// TO DOs:
+// filter by Subject ID
+// Add demographics table
 
-d3.json(url).then(d=>{
-    console.log(d)
-    let Samples = d.samples
-    
-    for (let i = 0; i < Samples.length; i++) {
-        var xValues = Samples[i].sample_values.slice(0,10)
-        var yValues = Samples[i].otu_ids.slice(0,10)
-        var hoverText = Samples[i].otu_labels.slice(0,10)
-    }
+    d3.json(URL).then(d=>{
 
-    yValuesString = yValues.map(x => `OTU ${x}`)
-    console.log(typeof(yValuesString))
-
-    console.log(yValues)
-    console.log(yValuesString)
-
-     let trace1 = {
-        x: xValues.reverse(),
-        y: yValuesString.reverse(),
-        text: hoverText.reverse(),
-        type:'bar',
-        orientation:'h'
-    }
-
-    let traceData = [trace1]
-
-    let layout = {
-        title: 'Top 10 OTUs'
-    }
-
-    Plotly.newPlot('plot', traceData, layout)
-    console.log(yValues)
-}) 
-
-
-
-// -----------------Bubble Chart--------------------
-
- d3.json(url).then(d=>{
-    console.log(d)
-    let Samples2 = d.samples
-    
-    for (let i = 0; i < Samples2.length; i++) {
-        var xValues2 = Samples2[i].sample_values.slice(0,10)
-        var yValues2 = Samples2[i].otu_ids.slice(0,10)
-        var hoverText2 = Samples2[i].otu_labels.slice(0,10)
-        // var color2 = 
-    }
-
-
-     let trace2 = {
-        x: yValues2,
-        y: xValues2,
-        text: hoverText2,
-        mode: 'markers',
-        marker: {
-            color: [xValues2],
-        //   size:[yValues2]   This displays the values blank
-          }
+        // -----------------Bar Chart--------------------
+        let Samples = d.samples
         
-    }
+        for (let i = 0; i < Samples.length; i++) {
+            var xValues = Samples[i].sample_values.slice(0,10)
+            var yValues = Samples[i].otu_ids.slice(0,10)
+            var hoverText = Samples[i].otu_labels.slice(0,10)
+        }
 
-    let traceData2 = [trace2]
+        yValuesString = yValues.map(x => `OTU ${x}`)
 
-    let layout2 = {
-        title: 'Scatter Plot'
-    }
+        let trace1 = {
+            // reversing so they can be ordered in descending
+            x: xValues.reverse(),
+            y: yValuesString.reverse(),
+            text: hoverText.reverse(),
+            type:'bar',
+            orientation:'h'
+        }
 
-    Plotly.newPlot('plot2', traceData2, layout2)
+        let traceData = [trace1]
 
-})  
+        let layout = {
+            title: 'Top 10 OTUs'
+        }
 
+        Plotly.newPlot('plot', traceData, layout)
+ 
+        // -----------------Bubble Chart--------------------
+        let Samples2 = d.samples
+        
+        for (let i = 0; i < Samples2.length; i++) {
+            var xValues2 = Samples2[i].sample_values.slice(0,10)
+            var yValues2 = Samples2[i].otu_ids.slice(0,10)
+            var hoverText2 = Samples2[i].otu_labels.slice(0,10)
+        }
 
-// -----------------Bar Chart Backup (dont delete)--------------------
+        let trace2 = {
+            x: yValues2,
+            y: xValues2,
+            text: hoverText2,
+            mode: 'markers',
+            marker: {
+                color: yValues2,
+            size: xValues2   
+            }
+            
+        }
 
-/*
-d3.json(url).then(d=>{
-    console.log(d)
-    let Samples2 = d.samples
-    
-    for (let i = 0; i < Samples2.length; i++) {
-        var xValues2 = Samples2[i].sample_values.slice(0,10)
-        var yValues2 = Samples2[i].otu_ids.slice(0,10)
-        var hoverText2 = Samples2[i].otu_labels.slice(0,10)
-    }
+        let traceData2 = [trace2]
 
-    yValuesString2 = yValues2.map(x => `OTU ${x}`)
-    console.log(typeof(yValuesString2))
+        let layout2 = {
+            title: 'Scatter Plot'
+        }
+        // -----------------Demographics table--------------------
+        Plotly.newPlot('plot2', traceData2, layout2)
 
-    console.log(yValues2)
-    console.log(yValuesString2)
+        let Samples3 = d.samples
+        d3
+        .select('h3')
+        .selectAll('p')
+        .data(d)
+        .enter()
+        .append('p')
+        .text('test')
 
-     let trace2 = {
-        x: xValues2,
-        y: yValuesString2,
-        text: hoverText2,
-        type:'bar',
-        orientation:'h'
-    }
+        console.log(d)
+    }) 
+}
 
-    let traceData2 = [trace2]
+function initialize() {
+    // populate the dropdown menu
+    d3.json(URL).then(d=>{
+        var subjectIDs = d.names;
+        console.log(subjectIDs);
+        var select = d3.select('#selDataset');
+        for (let i = 0; i < subjectIDs.length; i++) {
+            select.append("option").attr("value",subjectIDs[i]).text(subjectIDs[i]);   
+        }
+    });
+    // draw the plots for the first subject
+    console.log(initialize);
 
-    let layout = {
-        title: 'Top 100 OTUs'
-    }
+}
 
-    Plotly.newPlot('plot2', traceData2, layout)
-    console.log(yValues)
-})  */
+initialize();
